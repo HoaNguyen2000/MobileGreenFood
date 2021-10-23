18ad0e3b82ad008486ad2f0cc79d282f5f80813b
Nguyễn Phước Hoà <76418497+HoaNguyen2000@users.noreply.github.com>
2021-10-23 19:03:46 +0700

    fix some bug and down minium sdk

--- Report weekly/ReportWeekly1/Code/app/src/main/java/com/example/democontainer/MainActivity.java
+++ Report weekly/ReportWeekly1/Code/app/src/main/java/com/example/democontainer/MainActivity.java
@@ -2,17 +2,21 @@ package com.example.democontainer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ProgressBar pgbStyle11, pgbStyle12, pgbStyle13, pgbStyle2;
     EditText etProcess;
     Button btnSetProcess, btnThread, btnIndeterminateMode, btnDialogRing, btnDiaglogProcess;
-    int progess;
+    AlertDialog alert;
+    int progress = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +38,62 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 pgbStyle2.setIndeterminate(false);
-                progess = Integer.parseInt( etProcess.getText().toString() );
-                pgbStyle2.setProgress(progess);
+                progress = Integer.parseInt( etProcess.getText().toString() );
+                pgbStyle2.setProgress(progress);
+                Toast.makeText(MainActivity.this, "Progress: "+ progress +"/100", Toast.LENGTH_LONG).show();
+            }
+        });
+        //Start thread loop to pgbStyle2
+        btnThread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progress = 0;
+                pgbStyle2.setIndeterminate(false);
+                setProgressValue(progress);
+            }
+        });
+        //On mode Indeterminate
+        btnIndeterminateMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pgbStyle2.setIndeterminate(true);
+                Toast.makeText(MainActivity.this, "On mode Indeterminate", Toast.LENGTH_LONG).show();
+            }
+        });
+        btnDialogRing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
             }
         });
+        btnDiaglogProcess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alert.setTitle("Alert Progress");
+                alert.setMessage("Loading...");
+                alert.show();
+            }
+        });
     }
+    private void setProgressValue(final int progress) {
+
+        // set the progress
+        pgbStyle2.setProgress(progress);
+        // thread is used to change the progress value
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                setProgressValue(progress + 5);
+
+            }
+        });
+        thread.start();
+    }
+
 }
