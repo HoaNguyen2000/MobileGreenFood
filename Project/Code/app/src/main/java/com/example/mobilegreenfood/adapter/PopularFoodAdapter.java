b9521ff2111de60cd15faae251cc912590dbf729
Nguyễn Phước Hoà <76418497+HoaNguyen2000@users.noreply.github.com>
2021-11-09 21:45:43 +0700

    redesign main activity

---
+++ Project/Code/app/src/main/java/com/example/mobilegreenfood/adapter/PopularFoodAdapter.java
@@ -0,0 +1,58 @@
+package com.example.mobilegreenfood.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mobilegreenfood.R;
+import com.example.mobilegreenfood.model.PopularFood;
+
+import java.util.List;
+
+public class PopularFoodAdapter extends RecyclerView.Adapter<PopularFoodAdapter.PopularFoodViewHolder> {
+    public static Context context;
+    public static List<PopularFood> popularFoodList;
+    public PopularFoodAdapter(Context context, List<PopularFood> popularFoodList) {
+        this.context = context;
+        this.popularFoodList = popularFoodList;
+    }
+    @NonNull
+    @Override
+    public PopularFoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.popular_food_row_item, parent, false);
+        return new PopularFoodViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PopularFoodViewHolder holder, int position) {
+        holder.foodImage.setImageResource(popularFoodList.get(position).getImageUrl());
+        holder.name.setText(popularFoodList.get(position).getName());
+        holder.price.setText(popularFoodList.get(position).getPrice());
+    }
+
+    @Override
+    public int getItemCount() {
+        return popularFoodList.size();
+    }
+
+    public static final class PopularFoodViewHolder extends RecyclerView.ViewHolder{
+        ImageView foodImage;
+        TextView name, price;
+
+
+
+
+        public PopularFoodViewHolder(@NonNull View itemView) {
+            super(itemView);
+            foodImage = itemView.findViewById(R.id.food_image);
+            price = itemView.findViewById(R.id.price);
+            name = itemView.findViewById(R.id.name);
+        }
+    }
+}
