b2735579c2490173f05c8945418e8e8ea857c010
Nguyễn Phước Hoà <76418497+HoaNguyen2000@users.noreply.github.com>
2021-11-10 10:19:03 +0700

    redesign login activity

--- Project/Code/app/src/main/java/com/example/mobilegreenfood/LoginActivity.java
+++ Project/Code/app/src/main/java/com/example/mobilegreenfood/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.mobilegreenfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
