f2175fe17dfe9f2a9bb311c3b1c19949524357e2
Nguyễn Phước Hoà <76418497+HoaNguyen2000@users.noreply.github.com>
2021-11-10 10:42:58 +0700

    add alert dialog when click textview forgotpassword

--- Project/Code/app/src/main/java/com/example/mobilegreenfood/LoginActivity.java
+++ Project/Code/app/src/main/java/com/example/mobilegreenfood/LoginActivity.java
@@ -2,25 +2,40 @@ package com.example.mobilegreenfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     Button btnLogin;
+    TextView tvForgotPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
         btnLogin = (Button) findViewById(R.id.btnLogin);
+        tvForgotPassword = (TextView) findViewById(R.id.tvForgotPassword);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+            }
+        });
+        tvForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder ab = new AlertDialog.Builder(LoginActivity.this);
+                ab.setTitle("Thông báo");
+                ab.setMessage("Vui lòng liên hệ amdin để được cấp lại mật khẩu\nSĐT: 039-998-899");
+                ab.setPositiveButton("OK", null);
+                ab.show();
             }
         });
     }
